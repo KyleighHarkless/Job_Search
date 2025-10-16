a64820bee90f2fdf5c0c1b28b13bb1e8485148c7
Kyleigh <candyrae2006@gmail.com>
2025-10-15 22:27:56 -0500

    Comment out java code to switch to python

--- Scraper.java
+++ Scraper.java
@@ -1,50 +1,50 @@
-package Job_Search;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+// package Job_Search;
+// import org.jsoup.Jsoup;
+// import org.jsoup.nodes.Document;
+// import org.jsoup.nodes.Element;
+// import org.jsoup.select.Elements;
 
-class Scraper {
-    public static void main(String[] args) {
-        System.out.println("\n\nHello, World from Scraper.java!\n\n");
-        welcome_text();
-        String xula_url = "https://www.xula.edu/about/mission-values.html";
-        String xula_mission_statement = scrape_university_website(xula_url);
+// class Scraper {
+//     public static void main(String[] args) {
+//         System.out.println("\n\nHello, World from Scraper.java!\n\n");
+//         welcome_text();
+//         String xula_url = "https://www.xula.edu/about/mission-values.html";
+//         String xula_mission_statement = scrape_university_website(xula_url);
 
-        System.out.println("\n\nXULA Mission Statement:\n" + xula_mission_statement);
-    }
-    static String scrape_university_website(String url) {
-        try {
+//         System.out.println("\n\nXULA Mission Statement:\n" + xula_mission_statement);
+//     }
+//     static String scrape_university_website(String url) {
+//         try {
             
-            return mission_statement;
+//             return mission_statement;
 
-        } catch (Exception e) {
-            System.out.println("An error occurred while scraping the website: " + e.getMessage());
-        }
-    }
+//         } catch (Exception e) {
+//             System.out.println("An error occurred while scraping the website: " + e.getMessage());
+//         }
+//     }
 
-    static void welcome_text() {
-        System.out.println("\t\t   _      _      _      _      _      _      _      _      _      _   \r\n" + //
-                        "\t\t _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_ \r\n" + //
-                        "\t\t(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)\r\n" + //
-                        "\t\t (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_) \r\n" + //
-                        "\t\t   _                                                              _   \r\n" + //
-                        "\t\t _( )_                                                          _( )_ \r\n" + //
-                        "\t\t(_ o _)                                                        (_ o _)\r\n" + //
-                        "\t\t (_,_)       __        __   _                          _        (_,_) \r\n" + //
-                        "\t\t   _         \\ \\      / /__| | ___ ___  _ __ ___   ___| |         _   \r\n" + //
-                        "\t\t _( )_        \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ |       _( )_ \r\n" + //
-                        "\t\t(_ o _)        \\ V  V /  __/ | (_| (_) | | | | | |  __/_|      (_ o _)\r\n" + //
-                        "\t\t (_,_)          \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___(_)       (_,_) \r\n" + //
-                        "\t\t   _                                                              _   \r\n" + //
-                        "\t\t _( )_                                                          _( )_ \r\n" + //
-                        "\t\t(_ o _)                                                        (_ o _)\r\n" + //
-                        "\t\t (_,_)                                                          (_,_) \r\n" + //
-                        "\t\t   _      _      _      _      _      _      _      _      _      _   \r\n" + //
-                        "\t\t _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_ \r\n" + //
-                        "\t\t(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)\r\n" + //
-                        "\t\t (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_) \n");
+//     static void welcome_text() {
+//         System.out.println("\t\t   _      _      _      _      _      _      _      _      _      _   \r\n" + //
+//                         "\t\t _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_ \r\n" + //
+//                         "\t\t(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)\r\n" + //
+//                         "\t\t (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_) \r\n" + //
+//                         "\t\t   _                                                              _   \r\n" + //
+//                         "\t\t _( )_                                                          _( )_ \r\n" + //
+//                         "\t\t(_ o _)                                                        (_ o _)\r\n" + //
+//                         "\t\t (_,_)       __        __   _                          _        (_,_) \r\n" + //
+//                         "\t\t   _         \\ \\      / /__| | ___ ___  _ __ ___   ___| |         _   \r\n" + //
+//                         "\t\t _( )_        \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ |       _( )_ \r\n" + //
+//                         "\t\t(_ o _)        \\ V  V /  __/ | (_| (_) | | | | | |  __/_|      (_ o _)\r\n" + //
+//                         "\t\t (_,_)          \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___(_)       (_,_) \r\n" + //
+//                         "\t\t   _                                                              _   \r\n" + //
+//                         "\t\t _( )_                                                          _( )_ \r\n" + //
+//                         "\t\t(_ o _)                                                        (_ o _)\r\n" + //
+//                         "\t\t (_,_)                                                          (_,_) \r\n" + //
+//                         "\t\t   _      _      _      _      _      _      _      _      _      _   \r\n" + //
+//                         "\t\t _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_ \r\n" + //
+//                         "\t\t(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)\r\n" + //
+//                         "\t\t (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_) \n");
 
-        System.out.println(">> This is a program or tool that automatically extracts data from websites by parsing the underlying HTML code <<\n");
-    }
-}
+//         System.out.println(">> This is a program or tool that automatically extracts data from websites by parsing the underlying HTML code <<\n");
+//     }
+// }
