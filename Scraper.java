caf18daca8216de5b3a3f72bfdc6c8f2b401b98a
Kyleigh <candyrae2006@gmail.com>
2025-10-15 19:15:35 -0500

    Create welcome message and explanation message

--- Scraper.java
+++ Scraper.java
@@ -3,5 +3,30 @@ package Job_Search;
 class Scraper {
     public static void main(String[] args) {
         System.out.println("\n\nHello, World from Scraper.java!\n\n");
+        display_text();
+    }
+    static void display_text() {
+        System.out.println("\t\t   _      _      _      _      _      _      _      _      _      _   \r\n" + //
+                        "\t\t _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_ \r\n" + //
+                        "\t\t(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)\r\n" + //
+                        "\t\t (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_) \r\n" + //
+                        "\t\t   _                                                              _   \r\n" + //
+                        "\t\t _( )_                                                          _( )_ \r\n" + //
+                        "\t\t(_ o _)                                                        (_ o _)\r\n" + //
+                        "\t\t (_,_)       __        __   _                          _        (_,_) \r\n" + //
+                        "\t\t   _         \\ \\      / /__| | ___ ___  _ __ ___   ___| |         _   \r\n" + //
+                        "\t\t _( )_        \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ |       _( )_ \r\n" + //
+                        "\t\t(_ o _)        \\ V  V /  __/ | (_| (_) | | | | | |  __/_|      (_ o _)\r\n" + //
+                        "\t\t (_,_)          \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___(_)       (_,_) \r\n" + //
+                        "\t\t   _                                                              _   \r\n" + //
+                        "\t\t _( )_                                                          _( )_ \r\n" + //
+                        "\t\t(_ o _)                                                        (_ o _)\r\n" + //
+                        "\t\t (_,_)                                                          (_,_) \r\n" + //
+                        "\t\t   _      _      _      _      _      _      _      _      _      _   \r\n" + //
+                        "\t\t _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_  _( )_ \r\n" + //
+                        "\t\t(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)(_ o _)\r\n" + //
+                        "\t\t (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_)  (_,_) \n");
+
+        System.out.println(">> This is a program or tool that automatically extracts data from websites by parsing the underlying HTML code <<");
     }
 }
