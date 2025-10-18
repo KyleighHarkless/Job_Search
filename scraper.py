80e1be057c38534537ff4df6ebd6c2f2cea0b182
Kyleigh <candyrae2006@gmail.com>
2025-10-18 16:07:09 -0500

    Add fake_jobs to fake_jobs.csv and fix website_scraper

--- scraper.py
+++ scraper.py
@@ -1,15 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
+from csv import writer
+import csv
 
 def main():
     welcome_text()
 
-    xula_mission_statement = scrape_university_website("https://www.xula.edu/about/mission-values.html","div","editorarea")
-    utah_mission_statement = scrape_university_website("https://www.usu.edu/president/mission-statement/", "p", "lead")
+    print(f"\nXULA's Mission Statement:")
+    strip_text(scrape_website("https://www.xula.edu/about/mission-values.html","div","editorarea"))
+    
+    print(f"\nUtah State's Mission Statement:")
+    strip_text(scrape_website("https://www.usu.edu/president/mission-statement/", "p", "lead"))
 
-    print(f"\nXULA's Mission Statement: \n'{str(xula_mission_statement)}'")
-    print(f"\nUtah State's Mission Statement: \n'{str(utah_mission_statement)}'")
+    # ---- Fake job listings ----
+    job_data = scrape_website("https://realpython.github.io/fake-jobs/", "div", "card-content")
+    with open("fake_jobs.csv", "w") as f:
+        csv_writer = writer(f)
+        csv_writer.writerow(["Title", " Company", " Location", " Date Posted"])
 
+        for job in job_data:
+            title = job.find("h2", class_="title").get_text(strip=True)
+            company = job.find("h3", class_="company").get_text(strip=True)
+            location = job.find("p", class_="location").get_text(strip=True)
+            date_posted = job.find("time")["datetime"]
+            csv_writer.writerow([title, company, location, date_posted])
+
+def scrape_website(url, element, class_name = None):
+    try:
+        headers = {"User-Agent":("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/120.0.0.0 Safari/537.36")}
+        
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  
+
+        soup = BeautifulSoup(response.content, "lxml")
+        scraped_text = soup.find_all(element, class_=class_name) if class_name else soup.find(element)
+
+        if scraped_text:
+            return scraped_text
+        else:
+            return "Statement not found."
+
+    except Exception as e:
+        return (f"An error occurred while scraping the website: {e}")
+    
+def strip_text(scraped_text):
+    for text in scraped_text:
+        print(text.get_text(strip=True))
+    
 def welcome_text():
     underscores = "_" * len(">> This is a program or tool that automatically extracts data from websites by parsing the underlying HTML code <<")
     print(underscores)
@@ -37,24 +75,7 @@ def welcome_text():
     )
     print(f">> This is a program or tool that automatically extracts data from websites by parsing the underlying HTML code << \n{underscores}")
 
-def scrape_university_website(url, element, class_name = None):
-    try:
-        headers = {"User-Agent":("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")}
-        
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  
 
-        soup = BeautifulSoup(response.text, "html.parser")
-        mission_paragraph = soup.find(element, class_=class_name) if class_name else soup.find(element)
-
-        if mission_paragraph:
-            mission_statement = mission_paragraph.get_text(strip=True)
-            return mission_statement
-        else:
-            return "Mission statement not found."
-
-    except Exception as e:
-        return (f"An error occurred while scraping the website: {e}")
 
 if __name__ == "__main__":
     main()
