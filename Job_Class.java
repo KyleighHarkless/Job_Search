33e43ab65ec65702de4147c720738a956e1f66c8
Kyleigh <candyrae2006@gmail.com>
2025-10-18 23:52:29 -0500

    Create job class

---
+++ Job_Class.java
@@ -0,0 +1,13 @@
+public class Job_Class {
+    String title;
+    String location;
+    String company;
+    String date;
+
+    public Job_Class(String title, String location, String company, String date) {
+        this.title = title;
+        this.location = location;
+        this.company = company;
+        this.date = date;
+    }
+}
